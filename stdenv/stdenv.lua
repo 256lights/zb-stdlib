3e5f0eefaf37c654ed6d27e263591dee119f02f6
Roxy Light <roxy@256lights.llc>
2025-04-27 11:30:58 -0700

    Add `stdenv.makeDerivation` function
    Updates #7

--- stdenv/stdenv.lua
+++ stdenv/stdenv.lua
@@ -71,4 +71,50 @@ function module.makeBootstrapDerivation(args)
   return makeDerivation(bash, { gcc, gnumake, bash, busybox }, args)
 end
 
+local function baseDeps(system)
+  local bash = import("../packages/bash/bash.lua")[system].stdenv
+  return bash, {
+    bash,
+    import("../packages/bzip2/bzip2.lua")[system].stdenv,
+    import("../packages/coreutils/coreutils.lua")[system].stdenv,
+    import("../packages/diffutils/diffutils.lua")[system].stdenv,
+    import("../packages/findutils/findutils.lua")[system].stdenv,
+    import("../packages/gawk/gawk.lua")[system].stdenv,
+    import("../packages/gnugrep/gnugrep.lua")[system].stdenv,
+    import("../packages/gnumake/gnumake.lua")[system].stdenv,
+    import("../packages/gnupatch/gnupatch.lua")[system].stdenv,
+    import("../packages/gnused/gnused.lua")[system].stdenv,
+    import("../packages/gnutar/gnutar.lua")[system].stdenv,
+    import("../packages/gzip/gzip.lua")[system].stdenv,
+    import("../packages/xz/xz.lua")[system].stdenv,
+  }
+end
+
+---Build a derivation using the standard environment without a C compiler.
+---@param args {
+---pname: string?,
+---version: string?,
+---system: string,
+---[string]: string|number|boolean|(string|number|boolean)[],
+---}
+---@return derivation
+function module.makeDerivationNoCC(args)
+  local bash, deps = baseDeps(args.system)
+  return makeDerivation(bash, deps, args)
+end
+
+---Build a derivation using the standard environment.
+---@param args {
+---pname: string?,
+---version: string?,
+---system: string,
+---[string]: string|number|boolean|(string|number|boolean)[],
+---}
+---@return derivation
+function module.makeDerivation(args)
+  local bash, deps = baseDeps(args.system)
+  deps[#deps+1] = import("../packages/gcc/gcc.lua")[args.system].bootstrap
+  return makeDerivation(bash, deps, args)
+end
+
 return module
