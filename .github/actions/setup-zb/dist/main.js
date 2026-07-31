3891fbcb7aa7e2fdc02fd5a93beca66a8479cb4b
Roxy Light <roxy@256lights.llc>
2026-07-31 16:55:37 -0700

    Tweak tsconfig.json

--- .github/actions/setup-zb/dist/main.js
+++ .github/actions/setup-zb/dist/main.js
@@ -1,7 +1,7 @@
-console.log("Hello, World!");
+"use strict";console.log("Hello, World!");
 /**
  * @license
  * Copyright 2026 The zb Authors
  * SPDX-License-Identifier: MIT
  */
-//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vc3JjL21haW4udHMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbIi8qKlxuICogQGxpY2Vuc2VcbiAqIENvcHlyaWdodCAyMDI2IFRoZSB6YiBBdXRob3JzXG4gKiBTUERYLUxpY2Vuc2UtSWRlbnRpZmllcjogTUlUXG4gKi9cblxuY29uc29sZS5sb2coJ0hlbGxvLCBXb3JsZCEnKTtcbiJdLAogICJtYXBwaW5ncyI6ICJBQU1BLFFBQVEsSUFBSSxlQUFlIiwKICAibmFtZXMiOiBbXQp9Cg==
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiLi4vc3JjL21haW4udHMiXSwKICAic291cmNlc0NvbnRlbnQiOiBbIi8qKlxuICogQGxpY2Vuc2VcbiAqIENvcHlyaWdodCAyMDI2IFRoZSB6YiBBdXRob3JzXG4gKiBTUERYLUxpY2Vuc2UtSWRlbnRpZmllcjogTUlUXG4gKi9cblxuY29uc29sZS5sb2coJ0hlbGxvLCBXb3JsZCEnKTtcbiJdLAogICJtYXBwaW5ncyI6ICJhQU1BLFFBQVEsSUFBSSxlQUFlIiwKICAibmFtZXMiOiBbXQp9Cg==
